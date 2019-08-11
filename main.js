ecc5fbdf6a9cc31a5b9eff1a34fa7092f4d0b378
katmorris8 <kathryn.leigh.morris@gmail.com>
2019-08-11 17:18:37 -0400

    Commented out the toggle hover styling and added a click function on the toggle.

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.toggle');
+const toggleBtn = document.querySelector('.toggle-button');
+
+toggle.addEventListener('click', handleToggle);
+
+function handleToggle() {
+  // remove and add classes that style the toggle switch
+}
