5ebbd4a1b70d2b6cd921717bdcb9b55525fd95b7
katmorris8 <kathryn.leigh.morris@gmail.com>
2019-08-13 00:14:32 -0400

    Changed the text color to white for dark mode.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const body = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
 const toggleBtn = document.querySelector('.toggle-button');
 const links = document.querySelectorAll('.light-mode-link');
+const paragraphs = document.querySelectorAll('.light-mode-text');
 
 toggle.addEventListener('click', handleToggle);
 
@@ -15,10 +16,15 @@ function handleToggle() {
     toggleBtn.classList.add('dark-mode-toggle');
     // changes to the background color
     body.id = 'dark-mode';
-    //changes to the paragraph text color
+    // changes to the link color on hover
     for (let i = 0; i < links.length; i++) {
       links[i].classList.remove('light-mode-link');
       links[i].classList.add('dark-mode-link');
+    }
+    // changes to the paragraph text color
+    for (let i = 0; i < paragraphs.length; i++) {
+      paragraphs[i].classList.remove('light-mode-text');
+      paragraphs[i].classList.add('dark-mode-text');
     }
 
   // turn light mode on
@@ -30,7 +36,7 @@ function handleToggle() {
     toggleBtn.classList.add('light-mode-toggle');
     // changes to the background color
     body.id = 'light-mode';
-    //changes to the paragraph text color
+    // changes to the link color on hover
     for (let i = 0; i < links.length; i++) {
       links[i].classList.remove('dark-mode-link');
       links[i].classList.add('light-mode-link');
@@ -41,12 +47,11 @@ function handleToggle() {
 // light mode 
 // class light-mode-hover:
 // p color #333333
-// a hover color #333333
 
 // dark mode 
 // class dark-mode-hover:
 // p color white
-// a hover color white
 
 // create a transition for the background color
 // background color transition doesn't work
+// paragraph transition only works on first click
