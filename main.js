32d2e2f565d34391358ad031fc61fe96b15df248
katmorris8 <kathryn.leigh.morris@gmail.com>
2019-08-12 21:08:13 -0400

    Added a light mode class to the light/dark mode toggle and styled accordingly.

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ toggle.addEventListener('click', handleToggle);
 function handleToggle() {
   // remove and add classes that style the toggle switch
 }
+
+// light mode 
+// toggle button postition right .2rem
+// toggle background color white
+// toggle border color #333333
+
+// dark mode 
+// toggle button postition left .2rem
+// toggle background color #333333
+// toggle border color #cccccc
