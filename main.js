9938f8e5ae631dafb76317c90ba06add090aab88
katmorris8 <kathryn.leigh.morris@gmail.com>
2019-08-12 23:48:42 -0400

    Changed the background color from light to dark when the respective mode is triggered with the toggle button.

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
+const body = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
 const toggleBtn = document.querySelector('.toggle-button');
 
 toggle.addEventListener('click', handleToggle);
 
 function handleToggle() {
-  // dark mode
-  if (toggle.classList.contains('toggle-light')) {
+  // turn dark mode on
+  if (body.id === 'light-mode') {
     // changes to the toggle button
     toggle.classList.remove('toggle-light');
     toggle.classList.add('toggle-dark');
     toggleBtn.classList.remove('light-mode-toggle');
     toggleBtn.classList.add('dark-mode-toggle');
+    // changes to the background color
+    body.id = 'dark-mode';
 
-  // light mode
+  // turn light mode on
   } else if (toggle.classList.contains('toggle-dark')) {
     // changes to the toggle button
     toggle.classList.remove('toggle-dark');
     toggle.classList.add('toggle-light');
     toggleBtn.classList.remove('dark-mode-toggle');
     toggleBtn.classList.add('light-mode-toggle');
+    // changes to the background color
+    body.id = 'light-mode';
   }
 }
 
@@ -33,3 +38,6 @@ function handleToggle() {
 // class dark-mode-hover:
 // p color white
 // a hover color white
+
+// create a transition for the background color
+// background color transition doesn't work
