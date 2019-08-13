d1cd077907bce9272aba5726e1cde70195938f58
katmorris8 <kathryn.leigh.morris@gmail.com>
2019-08-12 23:32:16 -0400

    Added a hover class for dark mode.

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const toggleBtn = document.querySelector('.toggle-button');
 toggle.addEventListener('click', handleToggle);
 
 function handleToggle() {
+  // dark mode
   if (toggle.classList.contains('toggle-light')) {
     // changes to the toggle button
     toggle.classList.remove('toggle-light');
     toggle.classList.add('toggle-dark');
     toggleBtn.classList.remove('light-mode-toggle');
     toggleBtn.classList.add('dark-mode-toggle');
+
+  // light mode
   } else if (toggle.classList.contains('toggle-dark')) {
     // changes to the toggle button
     toggle.classList.remove('toggle-dark');
