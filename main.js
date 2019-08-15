fd4e94e49f10171afab6081e48d1d1aaf691497f
katmorris8 <kathryn.leigh.morris@gmail.com>
2019-08-14 22:31:50 -0400

    Removed the background color of the light/dark mode toggle for both light and dark mode since the body has the same background color in both modes.

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ const paragraphs = document.querySelectorAll('.light-mode-text');
 
 toggle.addEventListener('click', handleToggle);
 
+window.onload = handleToggle()
+
 function handleToggle() {
+  console.log(window.localStorage.getItem('mode'));
   // turn dark mode on
-  if (body.id === 'light-mode') {
+  if (window.localStorage.getItem('mode') === 'light' || body.id === 'light-mode') {
     // changes to the toggle button
     toggle.classList.remove('toggle-light');
     toggle.classList.add('toggle-dark');
@@ -26,9 +29,11 @@ function handleToggle() {
       paragraphs[i].classList.remove('light-mode-text');
       paragraphs[i].classList.add('dark-mode-text');
     }
+    // set local storage to dark mode
+    window.localStorage.setItem('mode', 'dark');
 
   // turn light mode on
-  } else if (body.id === 'dark-mode') {
+  } else if (window.localStorage.getItem('mode') === 'dark' || body.id === 'dark-mode') {
     // changes to the toggle button
     toggle.classList.remove('toggle-dark');
     toggle.classList.add('toggle-light');
@@ -46,5 +51,12 @@ function handleToggle() {
       paragraphs[i].classList.remove('dark-mode-text');
       paragraphs[i].classList.add('light-mode-text');
     }
+    // set local storage to dark mode
+    window.localStorage.setItem('mode', 'light');
   }
 }
+
+// save mode to local storage
+// adjust toggle size for mobile
+// make the headshot, gradient filter and blue shape strict sizes
+// change toggle symbols to fontawesome icons
